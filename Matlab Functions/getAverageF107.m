8ccfc0a8f1ece414e4db511ec0691a483a3ee246
kmehta282 <mehta282@purdue.edu>
2025-01-30 20:32:00 -0500

    Update F10.7 Data Every time Main function is run from online NOAA source.

--- Matlab Functions/getAverageF107.m
+++ Matlab Functions/getAverageF107.m
@@ -1,6 +1,8 @@
 function [average] = getAverageF107()
 
-rawdata = readmatrix('historicalF107DATA.txt');
+historicalF107DATA = websave('historicalF107DATA.txt','https://www-app3.gfz-potsdam.de/kp_index/Kp_ap_Ap_SN_F107_since_1932.txt');
+rawdata = readmatrix('historicalF107Data.txt','CommentStyle','#');
 f107data = rawdata((end-81:end), 26);
 average = mean(f107data);
+
 end
