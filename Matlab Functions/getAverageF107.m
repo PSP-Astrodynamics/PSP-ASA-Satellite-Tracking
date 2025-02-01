e8944f28382682cca0d542328cb637659646a108
kmehta282 <mehta282@purdue.edu>
2025-02-01 15:38:35 -0500

    Pulls daily solar and magnetic data directly from online source instead of having to manually input data.

--- Matlab Functions/getAverageF107.m
+++ Matlab Functions/getAverageF107.m
@@ -1,8 +1,10 @@
-function [average] = getAverageF107()
+function [daily, average, index] = getAverageF107()
 
-historicalF107DATA = websave('historicalF107DATA.txt','https://www-app3.gfz-potsdam.de/kp_index/Kp_ap_Ap_SN_F107_since_1932.txt');
+websave('historicalF107DATA.txt','https://www-app3.gfz-potsdam.de/kp_index/Kp_ap_Ap_SN_F107_since_1932.txt');
 rawdata = readmatrix('historicalF107Data.txt','CommentStyle','#');
 f107data = rawdata((end-81:end), 26);
+daily = rawdata(end,26);
 average = mean(f107data);
+index = rawdata(end,24);
 
 end
