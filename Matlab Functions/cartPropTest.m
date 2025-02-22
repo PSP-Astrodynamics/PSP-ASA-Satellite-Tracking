f5501eeb8b2909b3e114b347f1499c02dc19ddb5
evanpaull1 <evancpaull@gmail.com>
2025-02-22 15:00:51 -0500

    Update cartPropTest.m

--- Matlab Functions/cartPropTest.m
+++ Matlab Functions/cartPropTest.m
@@ -1,15 +1,15 @@
-clear; clc; close
+clear; clc; close all
 
 %% Initializations
-a0 = 6.8155e6; %semi major axis [km]
+a0 = 6.8155e3; %semi major axis [km]
 ecc0 = 7.148e-4; %eccentricity
 inc0 = deg2rad(97.3566); %inclination [rad]
 raan0 = deg2rad(95.9229); %Right Ascension of the Ascending Node [rad]
 argp0 = deg2rad(342.7667); %Argument of periapsis [rad]
-nu0 = 15.4297; %mean motion [revs / day]
+nu0 = 2*pi*15.4297/86400; %mean motion [rad / sec]
 simT = 0:0.01:86400*7; %simulation time [s]
 mu = 3.986004415e5; %Earth gravitational parameter [km^3/s^2]
-opt = odeset('RelTol',1e-10, 'AbsTol', 1e-10); %set options for ODE solver
+opt = odeset('RelTol',1e-13, 'AbsTol', 1e-13); %set options for ODE solver
 pert = {@noPert,@SRPpert};
 
 %% Calculations/Function Calls
